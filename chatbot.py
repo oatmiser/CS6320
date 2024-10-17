59cd5cb6caab0b58f28094868ecd27eb28ea46a0
Nathaniel Pott <116128673+oatmiser@users.noreply.github.com>
2024-10-17 02:39:36 -0500

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,29 @@
+class UserPlan:
+    def __init__(self, name:str, budget:float):
+        self.name = name
+        self.budget = budget
+    def setIngredients(self, food:list):
+        pass
+    def display(self):
+        s = f"Plan {self.name}:\n"
+        #print(s + f"\tbudget {self.budget}")
+        for attr in dir(self):
+            if not callable(getattr(self, attr)) and not attr.startswith("__"):
+                s += f"\t{attr} {getattr(self, attr)}\n"
+        print(s)
+
+plans = dict()
+def addPlan(name:str, budget:float):
+    global plans
+    plans[name] = UserPlan(name, budget)
+
+if __name__ == "__main__":
+    input_ = input("Enter a name and budget: ")
+    while input_ != "x":
+        name = " ".join(input_.split(" ")[0:-1])
+        budget = float(input_.split(" ")[-1])
+        addPlan(name, budget)
+        input_ = input("Enter a name and budget: ")
+
+    for p in plans.values():
+        p.display()
