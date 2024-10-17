a5157c4976c7834172b0b23e5baa79c0352c09c3
Nathaniel Pott <116128673+oatmiser@users.noreply.github.com>
2024-10-17 02:46:27 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,33 +2,42 @@ class UserPlan:
     def __init__(self, name:str, budget:float):
         self.name = name
         self.budget = budget
-    def setIngredients(self, food:list):
-        pass
+
     def display(self):
-        s = f"Plan {self.name}:\n"
-        #print(s + f"\tbudget {self.budget}")
+        str_build = f"Plan {self.name}:\n"
         for attr in dir(self):
             if not callable(getattr(self, attr)) and not attr.startswith("__"):
-                s += f"\t{attr} {getattr(self, attr)}\n"
-        print(s)
+                str_build += f"\t{attr} {getattr(self, attr)}\n"
+        print(str_build)
 
-plans = dict()
-def addPlan(name:str, budget:float):
-    global plans
-    plans[name] = UserPlan(name, budget)
+    def setIngredients(self, food:list):
+        pass
+
+    def setGoal(self, desc:str):
+        pass
+
+user_plans = dict()
+def add_plan(name:str, budget:float):
+    global user_plans
+    user_plans[name] = UserPlan(name, budget)
 
 if __name__ == "__main__":
-    while (True):
+    while True:
         user_choice = input("Command: ")
+        # don't use a switch so we can break easily?
         if user_choice == "QUIT":
             break
         elif user_choice == "NEW":
             args = input("Enter a name and budget: ")
             name = " ".join(args.split(" ")[0:-1])
             budget = float(args.split(" ")[-1])
-            addPlan(name, budget)
+            add_plan(name, budget)
         elif user_choice == "EDIT":
-            pass
+            search = input("Enter plan name: ")
+            if search in user_plans:
+                pass
+            else:
+                print("Sorry, this plan does not exist!")
         elif user_choice == "SHOW":
-            for p in plans.values():
-                p.display()
+            for obj in user_plans.values():
+                obj.display()
